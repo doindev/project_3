580c412cb70d678b24ec9efd21b5ea693fc773d8
doindev <javacoder26@gmail.com>
2025-09-16 11:41:16 -0500

    Update TelnetConstants.java

--- src/main/java/org/me/ibm/TelnetConstants.java
+++ src/main/java/org/me/ibm/TelnetConstants.java
@@ -17,9 +17,11 @@ public class TelnetConstants {
     public static final byte TERMINAL_TYPE = 24;
     public static final byte END_OF_RECORD = 25;
     public static final byte FORCE_LOGOUT = (byte) 0x12;
+    public static final byte EOR = (byte) 0xEF;   // eor
     
     // Terminal Type
     public static final String TERMINAL_TYPE_IBM3278 = "IBM-3278-2-E";
+    public static final String TERMINAL_TYPE_IBM3279 = "IBM-3279-2-E";
     
     // 3270 Command Codes
     public static final byte WRITE = (byte) 0xF1;
@@ -30,45 +32,45 @@ public class TelnetConstants {
     public static final byte READ_MODIFIED_ALL = (byte) 0x6E;
     
     // 3270 Orders
-    public static final byte SF = (byte) 0x1D;    // Start Field
-    public static final byte SFE = (byte) 0x29;   // Start Field Extended
-    public static final byte SBA = (byte) 0x11;   // Set Buffer Address
-    public static final byte SA = (byte) 0x28;    // Set Attribute
-    public static final byte MF = (byte) 0x2C;    // Modify Field
-    public static final byte IC = (byte) 0x13;    // Insert Cursor
-    public static final byte PT = (byte) 0x05;    // Program Tab
-    public static final byte RA = (byte) 0x3C;    // Repeat to Address
-    public static final byte EUA = (byte) 0x12;   // Erase Unprotected to Address
-    public static final byte GE = (byte) 0x08;    // Graphics Escape
+    public static final byte SF = 0x1D;    // Start Field
+    public static final byte SFE = 0x29;   // Start Field Extended
+    public static final byte SBA = 0x11;   // Set Buffer Address
+    public static final byte SA = 0x28;    // Set Attribute
+    public static final byte MF = 0x2C;    // Modify Field
+    public static final byte IC = 0x13;    // Insert Cursor
+    public static final byte PT = 0x05;    // Program Tab
+    public static final byte RA = 0x3C;    // Repeat to Address
+    public static final byte EUA = 0x12;   // Erase Unprotected to Address
+    public static final byte GE = 0x08;    // Graphics Escape
     
     // Write Control Characters
-    public static final byte WCC_RESET = (byte) 0x40;
-    public static final byte WCC_PRINT = (byte) 0x08;
-    public static final byte WCC_START_PRINTER = (byte) 0x04;
-    public static final byte WCC_SOUND_ALARM = (byte) 0x02;
-    public static final byte WCC_KEYBOARD_RESTORE = (byte) 0x01;
+    public static final byte WCC_KEYBOARD_RESTORE = 0x01; // bit 0
+    public static final byte WCC_SOUND_ALARM = 0x02; // bit 1
+    public static final byte WCC_START_PRINTER = 0x04; // bit 2
+    public static final byte WCC_PRINT = 0x08;// bit 3
+    public static final byte WCC_ERASE_ALL_UNPROTECTED = 0x40; // bit 6 or (byte)64
     
     // Field Attributes
-    public static final byte ATTR_PROTECTED = (byte) 0x20;
-    public static final byte ATTR_NUMERIC = (byte) 0x10;
-    public static final byte ATTR_DISPLAY_MASK = (byte) 0x0C;
-    public static final byte ATTR_INTENSITY_MASK = (byte) 0x08;
-    public static final byte ATTR_MDT = (byte) 0x01;  // Modified Data Tag
+    public static final byte ATTR_PROTECTED = 0x20;
+    public static final byte ATTR_NUMERIC = 0x10;
+    public static final byte ATTR_DISPLAY_MASK = 0x0C;
+    public static final byte ATTR_INTENSITY_MASK = 0x08;
+    public static final byte ATTR_MDT = 0x01;  // Modified Data Tag
     
     // Display Attributes
-    public static final byte DISPLAY_NORMAL = (byte) 0x00;
-    public static final byte DISPLAY_INVISIBLE = (byte) 0x08;
-    public static final byte DISPLAY_INTENSE = (byte) 0x04;
-    public static final byte DISPLAY_NONDISPLAY = (byte) 0x0C;
+    public static final byte DISPLAY_NORMAL = 0x00;
+    public static final byte DISPLAY_INVISIBLE = 0x08;
+    public static final byte DISPLAY_INTENSE = 0x04;
+    public static final byte DISPLAY_NONDISPLAY = 0x0C;
     
     // Structured Field Orders
-    public static final byte SF_READ_PARTITION = (byte) 0x01;
-    public static final byte SF_ACTIVATE_PARTITION = (byte) 0x0E;
-    public static final byte SF_DESTROY_PARTITION = (byte) 0x0F;
+    public static final byte SF_READ_PARTITION = 0x01;
+    public static final byte SF_ACTIVATE_PARTITION = 0x0E;
+    public static final byte SF_DESTROY_PARTITION = 0x0F;
     
     // AID (Attention Identifier) Codes
-    public static final byte AID_CLEAR = (byte) 0x6D;
-    public static final byte AID_ENTER = (byte) 0x7D;
+    public static final byte AID_CLEAR = 0x6D;
+    public static final byte AID_ENTER = 0x7D;
     public static final byte AID_PF1 = (byte) 0xF1;
     public static final byte AID_PF2 = (byte) 0xF2;
     public static final byte AID_PF3 = (byte) 0xF3;
